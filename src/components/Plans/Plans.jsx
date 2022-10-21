ef1d6f1d253c2d36e5d1bc59a55c456d1b56e25c
Eva <as45986@gmail.com>
2022-10-21 13:28:31 +0800

    feat:add plans part

--- src/components/Plans/Plans.jsx
+++ src/components/Plans/Plans.jsx
@@ -1,8 +1,39 @@
 import React from 'react'
+import './Plans.scss'
+import { plansData } from '../../data/plansData'
+import whiteTick from '../../assets/whiteTick.png'
 
 const Plans = () => {
   return (
-    <div>Plans</div>
+    <div className='plans-section'>
+      <div className="plan-heading">
+        <span>READY TO START </span>
+        <span className='stroke-text'>YOUR JOURNEY</span>
+        <span> NOW WITHUS</span>
+      </div>
+      <div className="plans">
+        {plansData.map((p, i) => (
+          <div className="plan" key={i}>
+            {p.icon}
+            <span>{p.name}</span>
+            <span>$ {p.price}</span>
+            <div className="features">
+              {p.features.map((feature, i) => (
+                <div className='feature'>
+                  <img src={whiteTick} alt="" />
+                  <span key={i}>{feature}</span>
+                </div>
+
+              ))}
+            </div>
+            <div>
+              <span>See more benefits -> </span>
+            </div>
+            <button className='btn'> Join Now</button>
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
 
